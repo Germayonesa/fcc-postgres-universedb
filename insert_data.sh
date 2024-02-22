e56f4e148a5e9f432f4e1cf9cbe8b740b347d4f0
Germayonesa <125098991+Germayonesa@users.noreply.github.com>
2024-02-22 13:50:21 -0600

    Update insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -12,20 +12,21 @@ cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPON
  do
   if [[ $YEAR != "year" ]]
    then
-    echo $YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS
-    if [[ $WINNER != "$($PSQL "SELECT name FROM teams WHERE name ='$WINNER'")" ]]
+
+    WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name ='$WINNER'")"
+    if [[ -z $WINNER_ID ]]
      then
-      "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")"
+      echo "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")"
     fi  
-    if [[ $OPPONENT != "$($PSQL "SELECT name FROM teams WHERE name='$OPPONENT'")" ]]
+
+    OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name ='$OPPONENT'")"
+    if [[ -z $OPPONENT_ID ]]
      then
-      "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")"
+      echo "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")"
      fi 
-    if [[ ($WINNER == "$($PSQL "SELECT name FROM teams WHERE name ='$WINNER'")") || ( $OPPONENT == "$($PSQL "SELECT name FROM teams WHERE name='$OPPONENT'")" ) ]] 
-     then
-      $WINNERID="$($PSQL "SELECT team_id FROM teams WHERE name ='$WINNER'")"
-      $OPPONENTID="$($PSQL "SELECT team_id FROM teams WHERE name ='$OPPONENT'")"
-      "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', "$($PSQL "SELECT team_id FROM teams WHERE name ='$WINNER'")", "$($PSQL "SELECT team_id FROM teams WHERE name ='$OPPONENT'")", $WINNER_GOALS, $OPPONENT_GOALS)")"
-     fi
+      WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name ='$WINNER'")"
+      OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name ='$OPPONENT'")"
+
+      "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")"
    fi
  done
