516281046ab30e12fe63568f4d8183bf587f5925
Germayonesa <125098991+Germayonesa@users.noreply.github.com>
2024-02-28 15:36:24 -0600

    Create salon.sh

---
+++ salon.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+echo -e "Welcome to my salon, how can i help you?\n"
+ 
+MAIN_MENU(){
+  if [[ $1 ]]
+   then
+    echo -e "\n$1"
+   fi
+  AVALIABLE_SERVICES=$($PSQL "SELECT service_id,name FROM services ORDER BY service_id")
+   echo $AVAILABLE_SERVICES
+  if [[ -z AVAILABLE_SERVICES ]]
+   then
+    echo "Sorry  we don't have any service available right now"
+   else
+    echo "$AVALIABLE_SERVICES" | while read SERVICE_ID BAR NAME
+   do 
+    echo "$SERVICE_ID) $NAME"
+   done
+   read SERVICE_ID_SELECTED
+    if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]]
+      then
+       MAIN_MENU "That is not a number"
+    else
+     SERV_AVAIL=$($PSQL "SELECT service_id FROM services WHERE service_id =$SERVICE_ID_SELECTED")
+     NAME_SERVICE=$($PSQL "SELECT name FROM services WHERE service_id =$SERVICE_ID_SELECTED")
+     if [[ -z $SERV_AVAIL ]]
+       then
+        MAIN_MENU "I could not find that service. what would you like today?"
+     else 
+       echo -e "\nWhat's your phone number?"
+       read CUSTOMER_PHONE
+       CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+       if [[ -z $CUSTOMER_NAME ]]
+         then
+           echo -e "\nI don't have a record for that phone number, what's your name?"
+           read CUSTOMER_NAME
+            INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone,name) VALUES('$CUSTOMER_PHONE','$CUSTOMER_NAME')")   
+         fi  
+      echo -e "\nWhat time would you like you $NAME_SERVICE, $CUSTOMER_NAME?"
+      read SERVICE_TIME
+      CUSTOMER_NAME_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+      if [[ $SERVICE_TIME ]]  
+       then
+        INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id,service_id,time) VALUES($CUSTOMER_NAME_ID, $SERVICE_ID_SELECTED,'$SERVICE_TIME')")
+        if [[ $INSERT_APPOINTMENT ]]  
+         then
+          echo -e "\nI have put you down for a $NAME_SERVICE at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ * | *$//g')."
+         fi
+       fi
+     fi
+    fi
+  fi
+ }  
+MAIN_MENU
